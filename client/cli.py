317b35c7981368c5445e17f914b1f4e3d3339d8f
毛林 <maolin@shanlitech.com>
2015-11-09 00:07:56 +0800

    use ip from config,not 127.0.0.1

--- client/cli.py
+++ client/cli.py
@@ -26,7 +26,7 @@ def get_timestamp():
 
 
 def main():
-    conf = json.load(file('%s/netester-cli.json' % current_path))
+    conf = json.load(file('%s/conf.json' % current_path))
     server = conf['server']
     #server = '127.0.0.1'
 

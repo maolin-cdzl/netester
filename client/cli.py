5ccce7955f4f2766bbf999da89222f8efb5d85dd
毛林 <mao.lin.cdzl@gmail.com>
2015-11-13 13:59:20 +0800

    support command line args

--- client/cli.py
+++ client/cli.py
@@ -30,10 +30,7 @@ current_path = os.path.abspath(os.path.dirname(module_path()))
 def get_timestamp():
     return int(round(time.time() * 1000.0));
 
-def main():
-    conf = json.load(file('%s/conf.json' % current_path))
-    server = conf['server']
-    #server = '127.0.0.1'
+def run_all(server,conf):
     print('Test from server: %s' % server)
 
     tester = Mtu(server,conf['MTU'])
@@ -57,6 +54,52 @@ def main():
     tester = TcpDelay(server,conf['TcpDelay'])
     tester.run()
 
+def main():
+    conf = json.load(file('%s/conf.json' % current_path))
+    server = conf['server']
+    #server = '127.0.0.1'
+
+    shortargs = 's:'
+    longargs = ['server=','MTU','NAT','DownloadSpeed','UploadSpeed','RtpDownStream','UdpDelay','TcpDelay']
+
+    opts,args = getopt.getopt(sys.argv[1:],shortargs,longargs)
+    for opt,val in opts:
+        if opt == '-s' or opt == '--server':
+            server = val
+
+    special = False
+    for opt,val in opts:
+        if opt == '--MTU':
+            special = True
+            tester = Mtu(server,conf['MTU'])
+            tester.run()
+        elif opt == '--NAT':
+            special = True
+            tester = Nat(server,conf['NAT'])
+            tester.run()
+        elif opt == '--DownloadSpeed':
+            special = True
+            tester = DownloadSpeed(server,conf['DownloadSpeed'])
+            tester.run()
+        elif opt == '--UploadSpeed':
+            special = True
+            tester = UploadSpeed(server,conf['UploadSpeed'])
+            tester.run()
+        elif opt == '--RtpDownStream':
+            special = True
+            tester = UdpDelay(server,conf['UdpDelay'])
+            tester.run()
+        elif opt == '--UdpDelay':
+            special = True
+            tester = UdpDelay(server,conf['UdpDelay'])
+            tester.run()
+        elif opt == '--TcpDelay':
+            special = True
+            tester = TcpDelay(server,conf['TcpDelay'])
+            tester.run()
+
+    if not special:
+        run_all()
 
 if __name__ == '__main__':
     main()
